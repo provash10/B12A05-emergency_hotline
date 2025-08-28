af4de18d26239311c51ac801057c59a41798c82f
provash10 <provash20cb@gmail.com>
2025-08-28 18:05:33 +0600

    Heart icon click onclick increaseHeart

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
-document.getElementById ("heart-icon-click")
-.addEventListener('click', function(e){
-    e.preventDefault   
+// document.getElementById ("heart-icon-click")
+// .addEventListener('click', function(e){
+//     e.preventDefault()   
 
+//     const heart = document.getElementById('navbar-heart-number').innerText
+//     const heartNumber = parseInt(document.getElementById('navbar-heart-number').innerText)
+//     const newHeartNumber = heartNumber + 1
+//     const totalHeart = document.getElementById('navbar-heart-number').innerText = newHeartNumber
+//     console.log(totalHeart) 
+// })
+
+function increaseHeart(){
     const heart = document.getElementById('navbar-heart-number').innerText
     const heartNumber = parseInt(document.getElementById('navbar-heart-number').innerText)
-
     const newHeartNumber = heartNumber + 1
-
     const totalHeart = document.getElementById('navbar-heart-number').innerText = newHeartNumber
-
-    console.log(totalHeart)
-
-    
-})
+    console.log(totalHeart) 
+}
