86b8b183ad89f43622011c1e517af3fb34c9cadb
provash10 <provash20cb@gmail.com>
2025-08-28 10:50:51 +0600

    Again design check and edit & complete

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("heart-icon-click")
+.addEventListener('click', function(e){
+    e.preventDefault
+    console.log('can i do it?')
+    navbarHeart = [0]
+    const navbarHeart = document.getElementById('navbar-heart')
+    const navbarHeartCount = navbarHeart.value + 1
+    console.log(navbarHeartCount).value
+})
