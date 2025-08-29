0d877825f1f72c52e0c2c3f8347cc6a876b3394e
provash10 <provash20cb@gmail.com>
2025-08-29 11:29:29 +0600

    Call Button,Alert, History, Clear

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # B12A05-emergency_hotline
+
+6. 
+1. getElementById         ---> use only unique element 
+getElementsByClassName --->>> use to same element 
+querySelector / querySelectorAll --->>> use matching element
+
+2. Create ---> document.createElement()
+   insert ----> parent.appendChild()
+const newDiv = document.createElement("div")
+document.body.appendChild(newDiv)
+
+.body--->parent
+
+3.  Event Bubbling --> after clicking it is going from child to parent
+
+4. Event Delegation --> setup Event Delegation to control child element and useful for dynamic.
+
+5. preventDefault() → stops default browser action.
+   stopPropagation() → stops event from bubbling up the DOM.
--- js/script.js
+++ js/script.js
@@ -1,18 +1,55 @@
-// document.getElementById ("heart-icon-click")
-// .addEventListener('click', function(e){
-//     e.preventDefault()   
-
-//     const heart = document.getElementById('navbar-heart-number').innerText
-//     const heartNumber = parseInt(document.getElementById('navbar-heart-number').innerText)
-//     const newHeartNumber = heartNumber + 1
-//     const totalHeart = document.getElementById('navbar-heart-number').innerText = newHeartNumber
-//     console.log(totalHeart) 
-// })
-
-function increaseHeart(){
-    const heart = document.getElementById('navbar-heart-number').innerText
-    const heartNumber = parseInt(document.getElementById('navbar-heart-number').innerText)
-    const newHeartNumber = heartNumber + 1
-    const totalHeart = document.getElementById('navbar-heart-number').innerText = newHeartNumber
-    console.log(totalHeart) 
+//click heart
+function increaseHeart() {
+    const heartCount = document.getElementById('navbar-heart-number')
+    let currentHeart = parseInt(heartCount.innerText)
+    currentHeart += 1
+    heartCount.innerText = currentHeart
+    console.log('Current heart:', currentHeart)
 }
+
+// Coins element, call button, History,clear
+const coinElement = document.getElementById("navbar-coin-number");
+const callButtons = document.querySelectorAll(".call-btn");
+const historyList = document.getElementById("history-list");
+const clearBtn = document.getElementById("clear-history-btn");
+
+// for Loop
+for (let i = 0; i < callButtons.length; i++) {
+  callButtons[i].addEventListener("click", function () {
+    let currentCoins = parseInt(coinElement.innerText);
+
+    if (currentCoins <= 20) {
+      alert("Conied Finished!");
+      return;
+    }
+
+    coinElement.innerText = currentCoins - 20;
+
+    // Service name & number showing
+    const card = callButtons[i].closest(".card"); 
+    const serviceName = card.querySelector(".service-name").innerText;
+    const serviceNumber = card.querySelector(".service-number").innerText;
+
+    alert(serviceName + " (" + serviceNumber + ")");
+
+
+    // div create
+    const historyItem = document.createElement("div");
+    historyItem.className = "p-2 border rounded-md bg-gray-100 text-sm md:text-base";
+
+    const now = new Date();
+    const dateTime = now.toLocaleString();
+
+    historyItem.innerText = serviceName + " - " + serviceNumber + " || " + dateTime;
+
+
+    //append 
+    historyList.appendChild(historyItem);
+  });
+}
+
+//clear history
+clearBtn.addEventListener("click", function () {
+  historyList.innerHTML = ""; 
+  console.log("History cleared");
+});
